da93432448df7361f2a921ef0b946140fa7bfa64
Teddy Knox <teddyknox@gmail.com>
2012-06-23 21:36:52 -0400

    init

---
+++ src/com/natarre/natarre/Home.java
@@ -0,0 +1,13 @@
+package com.natarre.natarre;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Home extends Activity {
+    
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home);
+    }
+}
